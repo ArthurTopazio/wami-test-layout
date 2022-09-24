bb24ef8bcb961c670e494814cc5bef44763cb503
ArthurTopazio <perereacmb@gmail.com>
2022-09-25 00:33:55 +0300

    create header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,21 @@
+import { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+
 import s from './Header.module.scss';
 
-const Header = () => {
+const Header: FC = () => {
   return (
     <div className={s.wrapper}>
-      Header
+      <div className={s.headerItems}>
+        <div className={s.label}>WAMISOFT</div>
+        <div className={s.navBar}>
+          <a>Services</a>
+          <a>Projects</a>
+          <a>About us</a>
+          <a>Contacts</a>
+          <a>Start project</a>
+        </div>
+      </div>
     </div>
   )
 }
